1c5f0d517ff4deb8701dd37c9eba9463f992bc9a
JimTheMan <mrdotjim@gmail.com>
2019-09-27 19:56:02 -0400

    well it certainly is working\!

--- route-controllers/Main.test.js
+++ route-controllers/Main.test.js
@@ -1,47 +1,55 @@
+jest.mock('../services/ClassDepService.js', () => {
+                            
+    const mockOverriddenOutput = "fake things, yay!"
+    return class MockClassDepService {
+        
+        doThings () {
+            console.log(mockOverriddenOutput)
+        }
+    }
+    
+});
 
-
-const rewiremock = require("rewiremock/node");
-rewiremock.overrideEntryPoint(module); // this is important
-
-const overriddenOutput = "fake things, yay!"
-
-const chai = require("chai"),
-    expect = require('chai').expect,
-    sinonChai = require('sinon-chai'),
-    calledWith = require('sinon-chai'),
-    sinon = require("sinon");
-
-chai.use(sinonChai);
-let consoleLogSpy
+const Main = require('./Main');
+let consoleLogSpy;
 
 describe("users route", () => {
-    before(() => {
-        consoleLogSpy = sinon.spy(console, "log");
-    });
-    after(() => {
-        console.log.restore()
-    });
+    beforeEach(() => {
+        // consoleLogSpy = sinon.spy(console, "log");
 
+        consoleLogSpy = jest.spyOn(console, 'log');
+    });
+    afterEach(() => {
+        // console.log.restore()
+        consoleLogSpy.mockRestore()
+    });
+    
     it("should call a function of its dependency's dependecy", () => {
-        const rewiremock = require("rewiremock/node");
-        const MockClassDepsService = class ClassDepService {
-            doThings() {
-                console.log(overriddenOutput);
-            }
-        }
-        MockClassDepsService['@noCallThru']
-
-        const mock = rewiremock.proxy(() => require("./../services/ClassService.js"), {
-            "./../services/ClassDepService": MockClassDepsService
-        });
-
-        const mainMock = rewiremock.proxy(() => require("./Main.js"), {
-            "./../services/ClassService.js": mock
-        });
-
-        main = new mainMock()
-
-        expect(consoleLogSpy).to.have.been.calledOnceWith(overriddenOutput);
-
-    });
-});
+        // const rewiremock = require("rewiremock/node");
+        // const MockClassDepsService = class ClassDepService {
+            //     doThings() {
+                //         console.log(overriddenOutput);
+                //     }
+                // }
+                // MockClassDepsService['@noCallThru']
+                
+                // const mock = rewiremock.proxy(() => require("./../services/ClassService.js"), {
+                    //     "./../services/ClassDepService": MockClassDepsService
+                    // });
+                    
+                    // const mainMock = rewiremock.proxy(() => require("./Main.js"), {
+                        //     "./../services/ClassService.js": mock
+                        // });
+                        
+                        // main = new mainMock()
+                        
+                    
+                        
+                        const expectedOverriddenOutput = "fake things, yay!"
+                        const main = new Main();
+                        
+                        expect(consoleLogSpy).toHaveBeenCalledWith(expectedOverriddenOutput);
+                        
+                    });
+                });
+                
